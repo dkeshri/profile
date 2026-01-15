d849a0bcf5bc39d0f03fd9c9387f897a1a88c410
dkeshri <dkeshridev@gmail.com>
2026-01-15 17:14:42 +0530

    angular 21

---
+++ web-app/src/app/components/base-layout/base-layout.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { Header } from "../header/header";
+import { Footer } from "../footer/footer";    
+@Component({
+  selector: 'app-base-layout',
+  imports: [ButtonModule, Header, Footer],
+  templateUrl: './base-layout.html',
+  styleUrl: './base-layout.css',
+})
+export class BaseLayout {
+
+}
