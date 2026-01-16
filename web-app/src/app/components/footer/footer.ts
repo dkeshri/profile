bf10a286f00fa6a982b3ca7f304c20c02b584438
dkeshri <dkeshridev@gmail.com>
2026-01-16 15:51:55 +0530

    update

--- web-app/src/app/components/footer/footer.ts
+++ web-app/src/app/components/footer/footer.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { SocialMediaIcons } from "../social-media-icons/social-media-icons";
 
 @Component({
@@ -7,6 +7,22 @@ import { SocialMediaIcons } from "../social-media-icons/social-media-icons";
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {
+export class Footer implements AfterViewInit {
   currentYear: number = new Date().getFullYear();
+
+
+  ngAfterViewInit() {
+  const observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        }
+      });
+    },
+    { threshold: 0.15 }
+  );
+
+  document.querySelectorAll('.fade-up').forEach(el => observer.observe(el));
+}
 }
