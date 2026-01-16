66afe7fd5ab93b45c97c8d70b22db954770094d9
dkeshri <dkeshridev@gmail.com>
2026-01-16 18:26:12 +0530

    update work experience

---
+++ web-app/src/app/components/work-experience-timeline/work-experience-timeline.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CompanyCard } from "../company-card/company-card";
+
+@Component({
+  selector: 'app-work-experience-timeline',
+  imports: [CompanyCard],
+  templateUrl: './work-experience-timeline.html',
+  styleUrl: './work-experience-timeline.css',
+})
+export class WorkExperienceTimeline {
+
+}
--- web-app/src/app/components/work-experience/work-experience.ts
+++ web-app/src/app/components/work-experience/work-experience.ts
@@ -1,34 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { WorkExperienceTimeline } from "../work-experience-timeline/work-experience-timeline";
 @Component({
   selector: 'app-work-experience',
-  imports: [],
+  imports: [ WorkExperienceTimeline],
   templateUrl: './work-experience.html',
   styleUrl: './work-experience.css',
 })
 export class WorkExperience implements OnInit {
 
   ngOnInit(): void {
-    // this.responsiveOptions = [
-    //         {
-    //             breakpoint: '1400px',
-    //             numVisible: 2,
-    //             numScroll: 1
-    //         },
-    //         {
-    //             breakpoint: '1199px',
-    //             numVisible: 2,
-    //             numScroll: 1
-    //         },
-    //         {
-    //             breakpoint: '767px',
-    //             numVisible: 1,
-    //             numScroll: 1
-    //         },
-    //         {
-    //             breakpoint: '575px',
-    //             numVisible: 1,
-    //             numScroll: 1
-    //         }
-    //     ]
   }
 }
