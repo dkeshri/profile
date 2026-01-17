563df439a11f6cd3adec04de4f2f217d2502f611
dkeshri <dkeshridev@gmail.com>
2026-01-17 10:46:47 +0530

    update not to scroll while mobile menu open

--- web-app/src/app/components/header/header.ts
+++ web-app/src/app/components/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { MobleMenu } from "../moble-menu/moble-menu";
 
 @Component({
@@ -8,8 +8,23 @@ import { MobleMenu } from "../moble-menu/moble-menu";
   styleUrl: './header.css',
 })
 export class Header {
+  mobileMenuOpenSignal = signal(false);
   mobileMenuOpen = false;
+  constructor() {
+    effect(() => {
+      if (this.mobileMenuOpenSignal()) {
+        document.body.style.overflow = 'hidden';
+      } else {
+        document.body.style.overflow = '';
+      }
+    });
+  }
   toggleMenu() {
     this.mobileMenuOpen = !this.mobileMenuOpen;
+    if(this.mobileMenuOpen){
+      this.mobileMenuOpenSignal.set(true);
+    } else {
+      this.mobileMenuOpenSignal.set(false);
+    }
   }
 }
