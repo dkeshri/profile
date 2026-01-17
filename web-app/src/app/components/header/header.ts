8f773d35904d5dbdefca72e0aab82e013878ad2c
dkeshri <dkeshridev@gmail.com>
2026-01-17 10:30:31 +0530

    update mobile menu

--- web-app/src/app/components/header/header.ts
+++ web-app/src/app/components/header/header.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { MobleMenu } from "../moble-menu/moble-menu";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MobleMenu],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
-
+  mobileMenuOpen = false;
+  toggleMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
 }
