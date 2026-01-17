8d942f8d337c16df013ae23ead4420e81746943d
dkeshri <dkeshridev@gmail.com>
2026-01-17 21:09:36 +0530

    update

--- web-app/src/app/components/skills/skills.ts
+++ web-app/src/app/components/skills/skills.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Skill from '../../models/skill';
+import { SkillCard } from "../skill-card/skill-card";
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [SkillCard],
   templateUrl: './skills.html',
   styleUrl: './skills.css',
 })
-export class Skills {
+export class Skills implements OnInit {
+  ngOnInit(): void {
+    this.skills = this.getSkills();
+  }
+  skills: Skill[] = [];
 
+  getSkills() {
+    return [
+      { 
+        title: 'Web / IoT Development', subtitle:'Full-stack & cloud-integrated applications',
+        favSkils: ['.NET Core', 'EF Core', 'Angular','MS SQL','Docker','Azure Cloud','InfluxDB'], 
+        nonFavSkills: ['ReactJS','ASP.NET Web Forms','Grafana','RabbitMQ'] 
+      },
+      { 
+        title: 'Tools / Platforms', subtitle:'Development & productivity tools',
+        favSkils: ['Azure DevOps', 'Git', 'GitHub','Visual Studio','VS Code','SSMS','Postman'], 
+        nonFavSkills: ['SVN','Figma'] 
+      },
+      { 
+        title: 'Programming', subtitle:'Core languages & fundamentals',
+        favSkils: ['C#', 'HTML', 'CSS','JavaScript','SQL','TypeScript'], 
+        nonFavSkills: [] 
+      },
+      
+    ] as Skill[];
+  }
 }
